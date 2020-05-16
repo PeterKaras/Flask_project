5cb3992e6bf67d49a30e479f4527123aa4730318
Flare7 <peter2001karas@gmail.com>
2020-05-16 15:47:59 +0000

    pokracovanie

--- myblog/app.py
+++ myblog/app.py
@@ -5,12 +5,13 @@ from flask import redirect
 from flask import url_for
 from flask import session
 from flask import g
+from flask import flash
 import os
 import sqlite3
 
 flask_app = Flask(__name__)
 flask_app.config.from_pyfile("/vagrant/configs/default.py")
-#flask_app.config.from_pyfile("/vagrant/configs/develop.py")
+flask_app.config.from_pyfile("/vagrant/configs/develop.py")
 
 if "MYBLOG_CONFIG" in os.environ:
     flask_app.config.from_envvar("MYBLOG_CONFIG")
@@ -26,6 +27,7 @@ def view_about():
 @flask_app.route("/admin/")
 def view_admin():
     if "logged" not in session:
+        flash("You must be logged in","alert-danger")
         return redirect(url_for("view_login"))
     return render_template("admin.jinja")
 
@@ -40,8 +42,9 @@ def view_articles():
 def add_article():
     db = get_db()
     db.execute("insert into articles (title,content) values (?,?)",
-                     [request.form.get("title"),request.form.get("content")])
+                [request.form.get("title"),request.form.get("content")])
     db.commit()
+    flash("Article was saved!","alert-success")
     return redirect(url_for("view_articles"))
 
 @flask_app.route("/articles/<int:art_id>/")
@@ -67,13 +70,16 @@ def view_user():
     if username == flask_app.config["USERNAME"] and \
        password == flask_app.config["PASSWORD"]:
         session["logged"] = True
+        flash("Login successful","alert-success")
         return redirect(url_for("view_admin"))
     else:
+        flash("Invalid credentials","alert-danger")
         return redirect(url_for("view_login"))
     
 @flask_app.route("/logout/",methods = ["POST"])
 def logout_user():
     session.pop("logged")
+    flash("Logout successful","alert-success")
     return redirect(url_for("view_welcome_page"))
 
 #utils
